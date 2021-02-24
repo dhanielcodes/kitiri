9012f640a052fe8dd220fa5dba05b5fd6262e4df
Adekoya Daniel <adekoyadaniel53@gmail.com>
2021-02-24 10:52:09 +0100

    update: tailwind and craco

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-center p-6 bg-black text-white">Kitiri Frontend</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
