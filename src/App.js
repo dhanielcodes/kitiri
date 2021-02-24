08f605087931f4d7e3f7d520b7d9b45ad9b9be02
Adekoya Daniel <adekoyadaniel53@gmail.com>
2021-02-24 16:45:46 +0100

    chore: postcss imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Home, About } from './views'
-
+import { Nav } from './components'
 
 function App() {
   return (
@@ -9,10 +9,7 @@ function App() {
       <div className="App">
       <header className="App-header">
         <h1 className="text-center p-6 bg-black text-white">Kitiri Frontend</h1>
-        <div className="flex justify-around text-blue py-6">
-          <Link className="cursor-pointer" to="/">Home</Link>
-          <Link className="cursor-pointer" to="/about">About</Link>
-        </div>
+        <Nav />
       </header>
       <Switch>
         <Route exact path="/">
