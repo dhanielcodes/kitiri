cdceb4c9638e0f534b1f528e69b81418e7b4cd0f
jomefavourite <jfjomefavourite@gmail.com>
2021-02-25 14:29:09 +0100

    fixed additional issues on the nav section
    Signed-off-by: jomefavourite <jfjomefavourite@gmail.com>

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,42 @@
 import '../App.css';
-import {HomeIcon, ProfileIcon, MessageIcon, SettingsIcon} from './Icons';
+import {
+  HomeIcon,
+  ProfileIcon,
+  MessageIcon,
+  SettingsIcon,
+  QuestionIcon,
+  LogoutIcon,
+  Logo,
+} from './Icons';
 import {Link} from 'react-router-dom';
 
 function Nav() {
   return (
-    <div className='flex justify-around py-6 bg-black'>
-      <Link className='cursor-pointer' to='/'>
-        <HomeIcon />
+    <div className='absolute bottom-5 left-0 right-0 mx-auto w-11/12 flex justify-around items-center py-6 bg-black rounded-3xl lg:px-12'>
+      <Link className='cursor-pointer hidden lg:block' to='/'>
+        <Logo />
       </Link>
-      <Link className='cursor-pointer' to='/about'>
-        <ProfileIcon />
-      </Link>
-      <Link className='cursor-pointer' to='/message'>
-        <MessageIcon />
-      </Link>
-      <Link className='cursor-pointer' to='/settings'>
-        <SettingsIcon />
+
+      <div className='flex justify-around items-center w-full max-w-3xl'>
+        <Link className='cursor-pointer' to='/'>
+          <HomeIcon />
+        </Link>
+        <Link className='cursor-pointer' to='/about'>
+          <ProfileIcon />
+        </Link>
+        <Link className='cursor-pointer' to='/message'>
+          <MessageIcon />
+        </Link>
+        <Link className='cursor-pointer' to='/settings'>
+          <SettingsIcon />
+        </Link>
+        <Link className='cursor-pointer hidden md:block' to='/help'>
+          <QuestionIcon />
+        </Link>
+      </div>
+
+      <Link className='cursor-pointer hidden lg:block' to='/logout'>
+        <LogoutIcon />
       </Link>
     </div>
   );
